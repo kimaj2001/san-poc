ddccb16561c145f14d01a8f057591175c5b485eb
Administrator <gitlab_admin_a06853@example.com>
2024-05-22 08:36:47 +0000

    Update file app.py

--- job/app.py
+++ job/app.py
@@ -13,6 +13,11 @@ def count_endpoint():
     count += 1
     return f'<h1 style="font-size: 3em;">Count: {count}</h1>'
 
+@app.route('/count-view')
+def count_endpoint():
+    global count
+    return f'<h1 style="font-size: 3em;">Count: {count}</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
