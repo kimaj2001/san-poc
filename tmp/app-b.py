2bf169887e3db2d62d4a05d2754db134b635dd6d
hwcho <hwcho@metanet.co.kr>
2024-05-23 16:38:58 +0900

    tmp

---
+++ tmp/app-b.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+count = 0
+
+@app.route('/')
+def hello():
+    return 'return f'<h1 style="font-size: 10em;"> B </h1>''
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
