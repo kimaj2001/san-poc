5b0bb42d566796c435d3c6719ef44e8eea4d13c9
hwcho <hwcho@metanet.co.kr>
2024-05-23 17:23:18 +0900

    ee

--- tmp/a/app.py
+++ tmp/a/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-count = 0
 
 @app.route('/')
 def hello():
-    return 'return f'<h1 style="font-size: 10em;"> A </h1>''
+    return f'<h1 style="font-size: 10em;"> A </h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
